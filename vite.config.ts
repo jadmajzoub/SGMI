aac22623c81c98053e76b6942c047eb7d8665744
leonardo-jesus <leonardojesus02@outlook.com>
2025-08-20 21:53:05 -0300

    feat: add env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: { port: 5173 }
+  server: { port: 3000 }
 })
